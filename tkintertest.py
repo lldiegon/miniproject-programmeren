39f18831de28a0e180e35098d09291ad2067a414
lldiegon <lldiegon@gmail.com>
2016-11-03 17:02:47 +0100

    Add files via upload

--- tkintertest.py
+++ tkintertest.py
@@ -67,50 +67,63 @@ def fiets_registreren():
                     def checkregistratie():
                         if str('@') not in email_entry or str('.') not in email_entry or len(email_entry) < 6 or len(email_entry) > 30:
                             emailcorrect = Label(master=subwindow, text='Dit email adres is niet geldig!', height=2)
-                            emailcorrect.pack()
+                            emailcorrect.grid(row=2, column=2)
 
                         if len(wachtwoord_entry) < 8 or len(wachtwoord_entry) > 12:
                             wachtwoordcorrect = Label(master=subwindow, text='Het gekozen wachtwoord moet minimaal 8 letters lang zijn en maximaal 12 letters lang!', height=2)
-                            wachtwoordcorrect.pack()
+                            wachtwoordcorrect.grid(row=3, column=2)
                         else:
                             writer.writerow((stickercode, voornaam_entry, achternaam_entry, wachtwoord_entry, email_entry))
                             del lijst[:]
 
-
-                    print('Uw stickercode is: ' + str(stickercode))
-
-
-
-
                     checkregistratieknop = Button(master=subwindow, text='Registreren', command=checkregistratie())
-                    checkregistratieknop.pack(pady=10)
-
-                    writer.writerow((stickercode, voornaam, achternaam, wachtwoord, email))
-                    del lijst[:]
+                    checkregistratieknop.grid(row=4, column=1)
 
                 else:
-                    print('Alle stalplaatsen zijn momenteel in gebruik, probeer het later opnieuw.')
+                    vol = Label(master=subwindow, text='Alle stalplaatsen zijn momenteel in gebruik, probeer het later opnieuw.')
+                    vol.grid(row=4, column=2)
 
             elif len(lijst) == None and len(lijst) < 50:
-                writer = csv.writer(schrijven, delimiter=';')
-                voornaam = str(input("Vul uw voornaam in: "))
-                achternaam = str(input("Vul uw achternaam in: "))
-
                 stickercode = randint(10000, 99999)
-                print('Uw stickercode is: ' + str(stickercode))
-                email = str(input("Vul uw email in: "))
 
-                while str('@') not in email or str('.') not in email or len(email) < 6 or len(email) > 30:
-                    print("Dit is geen geldig email adres!")
-                    email = str(input("Vul uw email in: "))
+                writer = csv.writer(schrijven, delimiter=';')
+                voornaam = Label(master=subwindow, text="Voornaam")
+                achternaam = Label(master=subwindow, text="Achternaam")
+                email = Label(master=subwindow, text="Email")
+                wachtwoord = Label(master=subwindow, text="wachtwoord")
+                stickercode = Label(master=subwindow, text="Uw unieke sticker code is: " + str(stickercode))
 
-                wachtwoord = str(input("Vul een wachtwoord in: "))
+                voornaam_entry = Entry(master=subwindow)
+                achternaam_entry = Entry(master=subwindow)
+                email_entry = Entry(master=subwindow)
+                wachtwoord_entry = Entry(master=subwindow, show="*")
 
-                while len(wachtwoord) < 8 or len(wachtwoord) > 12:
-                    print("Het gekozen wachtwoord moet minimaal 8 letters lang zijn en maximaal 12 letters lang!")
-                    wachtwoord = str(input("Vul een wachtwoord in: "))
+                voornaam.grid(row=0, sticky=E)
+                achternaam.grid(row=1, sticky=E)
+                email.grid(row=2, sticky=E)
+                wachtwoord.grid(row=3, sticky=E)
 
-                writer.writerow((stickercode, voornaam, achternaam, wachtwoord, email))
+                voornaam_entry.grid(row=0, column=1)
+                achternaam_entry.grid(row=1, column=1)
+                email_entry.grid(row=2, column=1)
+                wachtwoord_entry.grid(row=3, column=1)
+
+
+
+                def checkregistratie():
+                    if str('@') not in email_entry or str('.') not in email_entry or len(email_entry) < 6 or len(email_entry) > 30:
+                        emailcorrect = Label(master=subwindow, text='Dit email adres is niet geldig!', height=2)
+                        emailcorrect.grid(row=2, column=2)
+
+                    if len(wachtwoord_entry) < 8 or len(wachtwoord_entry) > 12:
+                        wachtwoordcorrect = Label(master=subwindow, text='Het gekozen wachtwoord moet minimaal 8 letters lang zijn en maximaal 12 letters lang!', height=2)
+                        wachtwoordcorrect.grid(row=3, column=2)
+                    else:
+                        writer.writerow((stickercode, voornaam_entry, achternaam_entry, wachtwoord_entry, email_entry))
+                        del lijst[:]
+
+                checkregistratieknop = Button(master=subwindow, text='Registreren', command=checkregistratie())
+                checkregistratieknop.grid(row=4, column=1)
 
 
 
